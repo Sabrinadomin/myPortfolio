2a79d9b866f681681f52880c73bd2c304d1b3ed4
Sabrina Conceicao <sabrinadomin14@gmail.com>
2022-12-20 14:12:21 -0300

    Added main image on Home page.

--- src/components/BackgroundImg/BackgroundImg.tsx
+++ src/components/BackgroundImg/BackgroundImg.tsx
@@ -1,9 +1,23 @@
 import React from "react"
 import "./BackgroundImgStyle.css"
+import mainImg from "../../assets/image-one.jpg"
+import { Link } from "react-router-dom"
 
 export default function BackgroundImg(): JSX.Element {
   return (
-    <div>
+    <div className="main">
+      <div className="mask">
+        <img className="background-img" src={mainImg} alt = "main-image"/>
+      </div>
+      <div className="content">
+          <p>{"Hi I'm Sabrina Conceição"}</p>
+          <h1>Backend Developer</h1>
+          <div>
+            <Link to="/projects" className="btn">Projects</Link>
+            <Link to="/contact" className="btn btn-light">Contact</Link>
+          </div>
+
+      </div>
 
     </div>
   )
