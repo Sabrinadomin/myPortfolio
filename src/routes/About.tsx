c7b1a7857c40eb55654f81ee63ad2f0b15a48e95
Sabrina Conceicao <sabrinadomin14@gmail.com>
2022-12-27 22:04:46 -0300

    Added about me styles.

--- src/routes/About.tsx
+++ src/routes/About.tsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import AboutContent from '../components/About/AboutContent'
 import BackgroundImg from '../components/BackgroundImg/Background/Background'
 
 export default function About(): JSX.Element {
   return (
-    <BackgroundImg heading = 'ABOUT ME_' text = 'Some stuff about me' page = 'about'/>
+    <>
+      <BackgroundImg heading = 'ABOUT ME_' text = 'Some stuff about me' page = 'about'/>
+      <AboutContent/>
+    </>
   )
 }
