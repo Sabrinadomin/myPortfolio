22effa8d440a7a1d9b9f0c592e3c13b64702c096
Sabrina Conceicao <sabrinadomin14@gmail.com>
2023-01-07 15:04:17 -0300

    Made the navbar dissapear when scroll down.

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,4 +1,5 @@
-import React, { useState } from "react"
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import React, { useEffect, useState } from "react"
 import "./NavbarStyles.css"
 import { Link } from "react-router-dom"
 import { FaBars, FaTimes } from "react-icons/fa"
@@ -6,34 +7,42 @@ import { FaBars, FaTimes } from "react-icons/fa"
 export default function Navbar(): JSX.Element {
   const [click, setClick] = useState(false)
   const handleClick = (): void => setClick(!click)
-  const [color, setColor] = useState(false)
 
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  function changeColor (): void {
-    if (window.scrollY >= 100) {
-      setColor(true)
+  const [show, setShow] = useState(true)
+  const controlNavbar = (): void => {
+    if (window.scrollY > 200) {
+      setShow(false)
     } else {
-      setColor(false)
+      setShow(true)
     }
   }
 
+  useEffect(() => {
+    window.addEventListener('scroll', controlNavbar)
+    return () => {
+      window.removeEventListener('scroll', controlNavbar)
+    }
+  }, [])
+
   return (
-    <div className={color ? "header header-bg" : "header"}>
-      <Link to="/">
-        <h1 className="title">Sabrina Conceição_</h1>
-      </Link>
-      <ul className = { click ? "nav-menu active" : "nav-menu"} onClick={handleClick}>
-        <li>
-          <Link to="/" >Home</Link>
-          <Link to="/about" >About</Link>
-          <Link to="/projects" >Projects</Link>
-          <Link to="/contact" >Contact</Link>
-        </li>
-      </ul>
-      <div className="barsMenu" onClick={handleClick}>
-        {click
-          ? (<FaTimes size = {20} style = {{ color: "#fff" }}/>)
-          : (<FaBars size = {20} style = {{ color: "#fff" }}/>)}
+    <div className={show ? 'nav' : 'nav-hidden'}>
+      <div className= "header">
+        <Link to="/">
+          <h1 className="title">Sabrina Conceição_</h1>
+        </Link>
+        <ul className = { click ? "nav-menu active" : "nav-menu"} onClick={handleClick}>
+          <li>
+            <Link className = "menu-item" to="/" >Home</Link>
+            <Link className = "menu-item" to="/about" >About</Link>
+            <Link className = "menu-item" to="/projects" >Projects</Link>
+            <Link className = "menu-item" to="/contact" >Contact</Link>
+          </li>
+        </ul>
+        <div className="barsMenu" onClick={handleClick}>
+          {click
+            ? (<FaTimes size = {20} style = {{ color: "#fff" }}/>)
+            : (<FaBars size = {20} style = {{ color: "#fff" }}/>)}
+        </div>
       </div>
     </div>
   )
